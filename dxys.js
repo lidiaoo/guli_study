011ed51bbd6b4eff5492668624accbcbdb9a0d2c
lidiaoo <49216602+lidiaoo@users.noreply.github.com>
2025-10-03 21:03:07 +0800

    Update dxys.js

--- dxys.js
+++ dxys.js
@@ -1,3 +1,12 @@
+/*
+丁香医生vip
+*************************** 
+[rewrite_local]
+^https:\//dxy.com\/app\/i\/user\/ask\/account url script-response-body https://raw.githubusercontent.com/lidiaoo/guli_study/refs/heads/master/dxys.js
+
+[mitm]
+hostname = dxy.com
+**************************/  
 console.log("++++++++茉莉花++++++++++");
 var body = $response.body.replace(/\"status\":9/, "\"status\":0").replace(/\"expire_timestamp\":0/, "\"expire_timestamp\":4102415999000");
 $done({body: body});
