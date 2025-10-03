614b76e9a2aee0cf370c22940c57cb29b5fff567
lidiaoo <49216602+lidiaoo@users.noreply.github.com>
2025-10-03 22:43:20 +0800

    Update dxys.js

--- dxys.js
+++ dxys.js
@@ -1,11 +1,2 @@
-/*
-丁香医生vip
-*************************** 
-[rewrite_local]
-^https:\//.*.com url script-response-body https://raw.githubusercontent.com/lidiaoo/guli_study/refs/heads/master/dxys.js
-
-[mitm]
-hostname = *.com
-**************************/  
 console.log("++++++++茉莉花++++++++++");
 $done({"1"});
