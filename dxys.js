8c1f71a184bc2892082e2d1a984c484b133d0a38
lidiaoo <49216602+lidiaoo@users.noreply.github.com>
2025-10-03 18:42:04 +0800

    Create dxys.js

---
+++ dxys.js
@@ -0,0 +1,13 @@
+/*
+丁香医生vip
+*************************** 
+[rewrite_local]
+^https:\//dxy.com\/app\/i\/user\/ask\/account url script-response-body https://raw.githubusercontent.com/lidiaoo/guli_study/refs/heads/master/dxys.js
+
+[mitm]
+hostname = dxy.com
+**************************/ 
+
+ 
+// 直接返回空字符串作为响应体
+$done({ body: "" });
