781a2fb9085fbdba3d98d9a02598e22c988eb493
lidiaoo <49216602+lidiaoo@users.noreply.github.com>
2025-10-03 18:21:10 +0800

    Update dxys.js

--- dxys.js
+++ dxys.js
@@ -9,6 +9,6 @@ QuantumultX:
 hostname = dxy.com
 **************************/
 var body = $response.body
-    .replace(/\"status\":9/, "\"status\":0")
-    .replace(/\"expire_timestamp\":0/, "\"expire_timestamp\":4102415999000");
+    .replaceAll(/\"status\":9/, "\"status\":0")
+    .replaceAll(/\"expire_timestamp\":0/, "\"expire_timestamp\":4102415999000");
 $done({ body });
