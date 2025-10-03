0e979a5d06212fe0f933546ad1f6326d24572484
lidiaoo <49216602+lidiaoo@users.noreply.github.com>
2025-10-03 22:53:29 +0800

    Update dxys.js

--- dxys.js
+++ dxys.js
@@ -1,2 +1,26 @@
-console.log("++++++++茉莉花++++++++++");
-$done({"1"});
+const url = $request.url;
+const method = $request.method;
+const notifyTitle = "dxy-json";
+console.log(`dxy.com json`);
+if (!$response.body) {
+    // 有undefined的情况
+    console.log(`$response.body为undefined:${url}`);
+    $done({});
+}
+if (method !== "GET") {
+    $notification.post(notifyTitle, "method错误:", method);
+}
+let body = JSON.parse($response.body);
+
+
+if (!body.data) {
+    console.log(url);
+    console.log(`body:${$response.body}`);
+    $notification.post(notifyTitle, url, "data字段错误");
+} else {
+}
+
+body = JSON.stringify(body);
+$done({
+    body
+});
