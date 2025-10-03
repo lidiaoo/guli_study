a581a5a28b9e081891aa097fa96dcb1fd4ee55a3
lidiaoo <49216602+lidiaoo@users.noreply.github.com>
2025-10-03 20:55:35 +0800

    Update dxys.js

--- dxys.js
+++ dxys.js
@@ -1,2 +1,3 @@
+console.log("++++++++茉莉花++++++++++");
 var body = $response.body.replace(/\"status\":9/, "\"status\":0").replace(/\"expire_timestamp\":0/, "\"expire_timestamp\":4102415999000");
 $done({body: body});
