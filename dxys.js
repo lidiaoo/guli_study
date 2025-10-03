33ae80f03525d9b7d5fa44ad603133ff3c23bc24
lidiaoo <49216602+lidiaoo@users.noreply.github.com>
2025-10-03 20:49:40 +0800

    Update dxys.js

--- dxys.js
+++ dxys.js
@@ -1,1 +1,10 @@
 
+let obj = JSON.parse($response.body);
+    obj.items[0].membership_info = {
+                    "status": 0,
+                    "expire_timestamp": 0,
+                    "type": 0,
+                    "trial_max": 0,
+                    "trial_received": 0
+  }
+$done({body: JSON.stringify(obj)});
