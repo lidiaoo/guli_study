342fcd7d3c127305620824f85c535f4178ae16eb
lidiaoo <49216602+lidiaoo@users.noreply.github.com>
2025-10-03 20:35:01 +0800

    Update dxys.js

--- dxys.js
+++ dxys.js
@@ -7,5 +7,12 @@
 [mitm]
 hostname = dxy.com
 **************************/  
-var body = $response.body;
-$done({body});  
+let obj = JSON.parse($response.body);
+    obj.items[0].membership_info = {
+                    "status": 0,
+                    "expire_timestamp": 0,
+                    "type": 0,
+                    "trial_max": 0,
+                    "trial_received": 0
+  }
+$done({body: JSON.stringify(obj)});
