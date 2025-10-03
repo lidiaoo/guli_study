afa9567965b68faffe8015ca75dd6836f8d65f92
lidiaoo <49216602+lidiaoo@users.noreply.github.com>
2025-10-03 18:27:32 +0800

    Update dxys.js

--- dxys.js
+++ dxys.js
@@ -7,10 +7,8 @@ QuantumultX:
 
 [mitm]
 hostname = dxy.com
-**************************/
-// 1. 先把服务器返回的 JSON 字符串（$response.body）转成 JS 对象，方便精准修改字段
-var bodyObj = "";
+**************************/ 
 
 
 // 4. 完成修改，返回新的响应体
-$done({ body: modifiedBody });
+$done({ body: {} });
