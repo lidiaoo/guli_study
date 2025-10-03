689df04d6edf004fd9c7bc26b7be356ba23193b3
lidiaoo <49216602+lidiaoo@users.noreply.github.com>
2025-10-03 20:41:09 +0800

    Update dxys.js

--- dxys.js
+++ dxys.js
@@ -1,14 +1,9 @@
-/*
-丁香医生vip
-***************************
-QuantumultX:
-[rewrite_local]
-^https:\//dxy.com\/app\/i\/user\/ask\/account url script-response-body https://raw.githubusercontent.com/lidiaoo/guli_study/refs/heads/master/dxys.js
-
-[mitm]
-hostname = dxy.com
-**************************/
-var body = $response.body
-    .replace(/\"status\":9/, "\"status\":0")
-    .replace(/\"expire_timestamp\":0/, "\"expire_timestamp\":4102415999000");
-$done({ body });
+let obj = JSON.parse($response.body);
+    obj.items[0].membership_info = {
+                    "status": 0,
+                    "expire_timestamp": 0,
+                    "type": 0,
+                    "trial_max": 0,
+                    "trial_received": 0
+  }
+$done({body: JSON.stringify(obj)});
